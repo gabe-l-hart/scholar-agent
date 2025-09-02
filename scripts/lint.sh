0a044210528b384b8a6876bfccc47679fbe93676
Gabe Goodhart <ghart@us.ibm.com>
2025-09-02 11:24:48 -0600

    build: Add dev scripts
    Branch: main
    
    Signed-off-by: Gabe Goodhart <ghart@us.ibm.com>

---
+++ scripts/lint.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+cd $(dirname ${BASH_SOURCE[0]})/..
+
+fnames=""
+for fname in $@
+do
+    if [[ "$fname" == *".py" ]] || [ -d $fname ] && [[ "$fname" == "src/scholar_agent"* ]]
+    then
+        fnames="$fnames $fname"
+    else
+        echo "Ignoring non-library file: $fname"
+    fi
+done
+if [ "$fnames" == "" ]
+then
+    fnames="src/scholar_agent"
+fi
+
+ruff check $arg $fnames
