1e654661637e17bd1877e56461f6f615655bb5d5
Gabe Goodhart <ghart@us.ibm.com>
2025-09-05 13:52:36 -0600

    feat: Initial work on crewai version
    Committed some things! It's very WIP, but it now boots multiple MCP
    servers correctly
    
    Branch: CrewAIHacking
    
    Signed-off-by: Gabe Goodhart <ghart@us.ibm.com>

---
+++ scholar/src/scholar/crew.py
@@ -0,0 +1,123 @@
+# Standard
+from typing import List
+import os
+
+# Third Party
+from crewai import Agent, Crew, Process, Task
+from crewai.agents.agent_builder.base_agent import BaseAgent
+from crewai.project import CrewBase, agent, crew, task
+from mcp import StdioServerParameters
+
+from scholar.tools.multi_mcp_adapter import (
+    MCPServerAdapterConfig,
+    MultiMCPServerAdapter,
+)
+
+# If you want to run a snippet of code before or after the crew starts,
+# you can use the @before_kickoff and @after_kickoff decorators
+# https://docs.crewai.com/concepts/crews#example-crew-class-with-decorators
+
+
+@CrewBase
+class Scholar:
+    """Scholar crew"""
+
+    agents: List[BaseAgent]
+    tasks: List[Task]
+
+    server_adapter: MultiMCPServerAdapter | None = None
+
+    def __init__(self, *_, **__):
+        """Override to also load mcp server configurations"""
+        mcp_config_path = self.base_directory / "config" / "mcp.yaml"
+        self.mcp_config = self.load_yaml(mcp_config_path)
+
+    def __del__(self):
+        if self.server_adapter:
+            self.server_adapter.stop()
+
+    @staticmethod
+    def _parse_mcp_config(server_config: dict) -> MCPServerAdapterConfig:
+        """Load the necessary inputs to MCPServerAdapter from config
+
+        TODO: Use pydantic for full validation
+        """
+        isolate = server_config.get("isolate", True)
+        transport = server_config.get("transport", "stdio")
+        if transport == "stdio":
+            env = os.environ.copy() if not isolate else {}
+            env.update(server_config.get("env", {}))
+            serverparams = StdioServerParameters(
+                command=server_config["command"],
+                args=server_config.get("args", []),
+                env=env,
+            )
+        elif transport == "sse":
+            serverparams = {"url": server_config["url"]}
+        else:
+            raise ValueError(f"Unsupported transport: {transport}")
+        return MCPServerAdapterConfig(
+            serverparams=serverparams,
+            isolate=isolate,
+            kwargs=server_config.get("kwargs", {}),
+        )
+
+    def _init_mcp(self):
+        if self.server_adapter is None:
+            adapter_configs = [
+                self._parse_mcp_config(cfg)
+                for cfg in self.mcp_config.get("mcpServers", {}).values()
+            ]
+            self.server_adapter = MultiMCPServerAdapter(*adapter_configs)
+
+    # Learn more about YAML configuration files here:
+    # Agents: https://docs.crewai.com/concepts/agents#yaml-configuration-recommended
+    # Tasks: https://docs.crewai.com/concepts/tasks#yaml-configuration-recommended
+
+    # If you would like to add tools to your agents, you can learn more about it here:
+    # https://docs.crewai.com/concepts/agents#agent-tools
+    @agent
+    def document_parser(self) -> Agent:
+        self._init_mcp()
+        return Agent(
+            config=self.agents_config["document_parser"],  # type: ignore[index]
+            verbose=True,
+        )
+
+    @agent
+    def research_citation_reviewer(self) -> Agent:
+        self._init_mcp()
+        return Agent(
+            config=self.agents_config["research_citation_reviewer"],  # type: ignore[index]
+            verbose=True,
+        )
+
+    # To learn more about structured task outputs,
+    # task dependencies, and task callbacks, check out the documentation:
+    # https://docs.crewai.com/concepts/tasks#overview-of-a-task
+    @task
+    def convert_document_task(self) -> Task:
+        return Task(
+            config=self.tasks_config["convert_document_task"],  # type: ignore[index]
+        )
+
+    @task
+    def find_background_topics_task(self) -> Task:
+        return Task(
+            config=self.tasks_config["find_background_topics_task"],  # type: ignore[index]
+        )
+
+    @crew
+    def crew(self) -> Crew:
+        """Creates the Scholar crew"""
+        # To learn how to add knowledge sources to your crew, check out the documentation:
+        # https://docs.crewai.com/concepts/knowledge#what-is-knowledge
+
+        return Crew(
+            agents=self.agents,  # Automatically created by the @agent decorator
+            tasks=self.tasks,  # Automatically created by the @task decorator
+            process=Process.sequential,
+            verbose=True,
+            # output_log_file="logs.json",
+            # process=Process.hierarchical, # In case you wanna use that instead https://docs.crewai.com/how-to/Hierarchical/
+        )
---
+++ scholar/src/scholar/tools/multi_mcp_adapter.py
@@ -0,0 +1,92 @@
+"""
+This module provides an abstraction over the built in MCPServerAdapter to hold
+a collection of MCP servers as a single set of tools. It is useful for managing
+multiple MCP servers as a single logical server.
+"""
+
+# Standard
+from dataclasses import dataclass, field
+import os
+import re
+import tempfile
+
+from crewai.tools import BaseTool
+from crewai_tools import MCPServerAdapter
+from crewai_tools.adapters.tool_collection import ToolCollection
+
+# Third Party
+from mcp import StdioServerParameters
+
+
+@dataclass
+class MCPServerAdapterConfig:
+    serverparams: StdioServerParameters | dict
+    kwargs: dict[str, any] = field(default_factory=dict)
+    isolate: bool = True
+
+
+class MultiMCPServerAdapter:
+    """This is wrapper around MCPServerAdapter that can adapt multiple MCP
+    servers into a single logical set of tools
+    """
+
+    def __init__(
+        self,
+        *adapter_configs: MCPServerAdapterConfig,
+        tool_filters: list[str] | None = None
+    ):
+        self._adapter_configs = adapter_configs
+        self._tool_filters = [re.compile(f) for f in (tool_filters or [])]
+        self._contexts = []
+        self._all_tools = []
+        self.start()
+
+    def _maybe_isolate(self, isolate: bool):
+        if isolate:
+            workdir = tempfile.TemporaryDirectory()
+            path = workdir.__enter__()
+            self._contexts.append(workdir)
+            os.chdir(path)
+
+    def _enter_adapter(self, adapter_config: MCPServerAdapterConfig):
+        self._maybe_isolate(adapter_config.isolate)
+        adapter = MCPServerAdapter(adapter_config.serverparams, **adapter_config.kwargs)
+        adapter.__enter__()
+        self._contexts.append(adapter)
+        self._all_tools.append(adapter.tools)
+
+    def _tool_predicate(self, tool: BaseTool) -> bool:
+        return not self._tool_filters or any(
+            f.match(tool.name) for f in self._tool_filters
+        )
+
+    def start(self):
+        """Start the MCP servers and initialize the tools."""
+        self.__enter__()
+
+    def stop(self):
+        """Stop the MCP servers"""
+        self.__exit__(None, None, None)
+
+    @property
+    def tools(self) -> ToolCollection[BaseTool]:
+        return ToolCollection(
+            [
+                tool
+                for collection in self._all_tools
+                for tool in collection.filter_where(self._tool_predicate)
+            ]
+        )
+
+    def __enter__(self) -> ToolCollection[BaseTool]:
+        for cfg in self._adapter_configs:
+            self._maybe_isolate(cfg.isolate)
+            self._enter_adapter(cfg)
+        return self.tools
+
+    def __exit__(self, *args, **kwargs) -> bool | None:
+        out_val = None
+        for ctx in self._contexts:
+            if ctx.__exit__(*args, **kwargs):
+                out_val = True
+        return out_val
